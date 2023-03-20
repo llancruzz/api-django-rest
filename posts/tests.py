373a42c67775dd8c80309c59abe6f7f9e1540d88
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-03-20 17:37:56 +0000

    chore: Added PostListViewTests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Post
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='luan', password='pass')
+
+    def test_can_list_posts(self):
+        luan = User.objects.get(username='luan')
+        Post.objects.create(owner=luan, title='a title')
+        response = self.client.get('posts/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username='luan', password='pass')
+        response = self.client.post('/posts/', {'title': 'a title'})
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        print(response.data)
+        print(len(response.data))
+
+    def test_user_not_logged_in_cant_create_post(self):
+        response = self.client.post('/posts/', {'title': 'a title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
