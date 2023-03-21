5b6eedba7660c85ce661ca169344bb6f82767d53
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-03-21 10:37:20 +0000

    :recycle: refactor: Refactored Postlist and PostDetail to generics views

--- posts/tests.py
+++ posts/tests.py
@@ -19,7 +19,7 @@ class PostListViewTests(APITestCase):
         response = self.client.post('/posts/', {'title': 'a title'})
         count = Post.objects.count()
         self.assertEqual(count, 1)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         print(response.data)
         print(len(response.data))
 
