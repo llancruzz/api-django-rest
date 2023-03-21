28d78df03efe107d9dcfa5da21b989bddff1eb09
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-03-21 09:04:59 +0000

    chore: Added LikeList and LikeDetail generics views

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    List all likes. Create a like if authenticated. 
+    The perform_create method associates the like with the logged in user.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like. No Update view, as users can only like or unlike a post. 
+    Destroy a like, i.e. unlike a post if owner of that like
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
